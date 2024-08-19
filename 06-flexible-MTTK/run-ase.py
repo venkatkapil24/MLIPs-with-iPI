9957dd3c957bbc3c626f71980e11ae08a12d82ad
Venkat Kapil <venkat.kapil@gmail.com>
2024-08-19 11:40:01 +0200

    cuda for run-ase and temperature for NVE

--- 06-flexible-MTTK/run-ase.py
+++ 06-flexible-MTTK/run-ase.py
@@ -5,11 +5,11 @@ from ase.calculators.socketio import SocketClient
 # Define atoms object
 print ("Reading atoms object.")
 atoms = read("init.xyz", 0)
-atoms.calc = mace_mp(device='cpu')
+atoms.calc = mace_mp()
 
 host = "driver"
 print ("Setting up socket.")
 client = SocketClient(unixsocket=host)
 print ("Running socket.")
-client.run(atoms)
+client.run(atoms, use_stress=True)
 print ("setting up calculator")
